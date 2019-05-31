fa68599dcba648318b423dde242b29eb7baf59e0
Samuel Albrecht <elsamuko@gmail.com>
2019-05-31 18:25:36 +0200

    add: check macros

--- src/utils.hpp
+++ src/utils.hpp
@@ -3,6 +3,11 @@
 #include <vector>
 #include <string>
 
+#include "log.hpp"
+
+#define CHECK( A ) if( !(A) ) { LOG( "Failed check for "#A ); }
+#define CHECK_EQ( A, B ) if( (A) != (B) ) { LOG( "Failed check for "#A" == "#B", " << A << " != " << B ); }
+
 namespace utils {
 uint8_t parseHex( const char& hex );
 std::vector<uint8_t> hexToBinary( const std::string& hex );
