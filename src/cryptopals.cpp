7f50811de123c8412062c2db66ec33ed42593759
Samuel Albrecht <elsamuko@gmail.com>
2019-05-31 19:17:56 +0200

    add: challenge 1.2

--- src/cryptopals.cpp
+++ src/cryptopals.cpp
@@ -10,9 +10,20 @@ void challenge1_1() {
     CHECK_EQ( expected, calculated );
 }
 
+// https://cryptopals.com/sets/1/challenges/2
+void challenge1_2() {
+    LOG( "Running challenge 1.2" );
+    std::string first = "1c0111001f010100061a024b53535009181c";
+    std::string second = "686974207468652062756c6c277320657965";
+    std::string expected = "746865206b696420646f6e277420706c6179";
+    std::string calculated = utils::XOR( first, second );
+    CHECK_EQ( expected, calculated );
+}
+
 int main() {
 
     challenge1_1();
+    challenge1_2();
 
     return 0;
 }
