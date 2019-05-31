0b6dfaefbcbbe89527450718eb8fb7bde1e56ed1
Samuel Albrecht <elsamuko@gmail.com>
2019-05-31 18:25:46 +0200

    chg: wrap first challenge

--- src/cryptopals.cpp
+++ src/cryptopals.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include "utils.hpp"
 
-int main() {
-    // https://cryptopals.com/sets/1/challenges/1
+// https://cryptopals.com/sets/1/challenges/1
+void challenge1_1() {
+    LOG( "Running challenge 1.1" );
     std::string hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-    std::cout << "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t" << std::endl;
+    std::string expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+    std::string calculated = utils::hexToBase64( hex );
+    CHECK_EQ( expected, calculated );
+}
 
-    std::string base64 = utils::hexToBase64( hex );
-    std::cout << base64 << std::endl;
+int main() {
+
+    challenge1_1();
 
     return 0;
 }
