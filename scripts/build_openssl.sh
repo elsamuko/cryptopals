74011a1fcd08169b85548ef078a74bd743f884f1
Samuel Albrecht <elsamuko@gmail.com>
2019-06-05 19:53:02 +0200

    add: win: build openssl for Windows

--- scripts/build_openssl.sh
+++ scripts/build_openssl.sh
@@ -26,6 +26,7 @@ PROJECT_DIR="$MAIN_DIR/tmp/$PROJECT"
 DOWNLOAD="$PROJECT_DIR/$PROJECT-$VERSION.tar.gz"
 SRC_DIR="$PROJECT_DIR/src"
 BUILD_DIR="$SRC_DIR/$PROJECT-$VERSION"
+BUILD_HELPER="$BUILD_DIR/build.bat"
 
 function indent {
     sed  's/^/     /'
@@ -53,7 +54,7 @@ function doUnzip {
     tar xzf "$DOWNLOAD" -C "$SRC_DIR"
 }
 
-function doConfigure {
+function doConfigureLinux {
     ./Configure > "$PROJECT_DIR/options.txt"
     case "$1" in
         'd'*)
@@ -68,7 +69,25 @@ function doConfigure {
     esac
 }
 
-function doBuild {
+function doConfigureWin {
+    local WBUILD_DIR="$(cygpath -w "$BUILD_DIR")"
+    perl Configure > "$PROJECT_DIR/options.txt"
+    case "$1" in 
+        'd'*)
+			echo "perl Configure no-asm no-shared threads debug-VC-WIN64A --openssldir=\"$WBUILD_DIR\\$1\" --prefix=\"$WBUILD_DIR\\$1\""
+            perl Configure no-asm no-shared threads debug-VC-WIN64A --openssldir="$WBUILD_DIR\\$1" --prefix="$WBUILD_DIR\\$1"
+            ;;
+        'r'*)
+			echo "perl Configure no-shared threads VC-WIN64A --openssldir=\"$WBUILD_DIR\\$1\" --prefix=\"$WBUILD_DIR\\$1\""
+            perl Configure no-asm no-shared threads VC-WIN64A --openssldir="$WBUILD_DIR\\$1" --prefix="$WBUILD_DIR\\$1"
+            ;;
+        *)
+            echo "Error in $LINENO : \$1 is $1"
+            ;;
+    esac
+}
+
+function doBuildLinux {
     cd "$SRC_DIR/$PROJECT-$VERSION"
 
     # debug
@@ -88,14 +107,51 @@ function doBuild {
     make install_sw
 }
 
+function createHelperWin {
+    local VERSIONS=("2019" "2017")
+    local EDITIONS=("BuildTools" "Community" "Professional" "Enterprise")
+
+    for VERSION in "${VERSIONS[@]}"; do
+        for EDITION in "${EDITIONS[@]}"; do
+            local VCVARS_DIR="C:/Program Files (x86)/Microsoft Visual Studio/$VERSION/$EDITION/VC/Auxiliary/Build"
+            if [ -d "$VCVARS_DIR" ]; then
+                export VSNEWCOMNTOOLS="${VCVARS_DIR////\\}"
+                break 2
+            fi
+        done
+    done
+
+    echo -ne '@echo off\r\n' > "$HELPER"
+    echo -ne "call \"$VSNEWCOMNTOOLS\\\\vcvars64.bat\"\r\n" >> "$BUILD_HELPER"
+	echo -ne 'nmake vclean\r\n' >> "$BUILD_HELPER"
+	echo -ne 'nmake \r\n' >> "$BUILD_HELPER"
+	echo -ne 'nmake install\r\n' >> "$BUILD_HELPER"
+
+    chmod +x "$BUILD_HELPER"
+}
+
+function doBuildWin {
+    cd "$SRC_DIR/$PROJECT-$VERSION"
+	
+    # debug
+    doConfigureWin debug
+	createHelperWin
+	"$BUILD_HELPER"
+
+    # release
+    doConfigureWin release
+	createHelperWin
+	"$BUILD_HELPER"
+}
+
 function doCopy {
     mkdir -p "$TARGET_DIR/bin/$OS/debug"
     mkdir -p "$TARGET_DIR/bin/$OS/release"
     mkdir -p "$TARGET_DIR/include"
-    cp -r "$BUILD_DIR/debug/lib/libcrypto.a" "$TARGET_DIR/bin/$OS/debug/libcrypto.a"
-    cp -r "$BUILD_DIR/debug/lib/libssl.a" "$TARGET_DIR/bin/$OS/debug/libssl.a"
-    cp -r "$BUILD_DIR/release/lib/libcrypto.a" "$TARGET_DIR/bin/$OS/release/libcrypto.a"
-    cp -r "$BUILD_DIR/release/lib/libssl.a" "$TARGET_DIR/bin/$OS/release/libssl.a"
+    cp -r "$BUILD_DIR/debug/lib/libcrypto."* "$TARGET_DIR/bin/$OS/debug/"
+    cp -r "$BUILD_DIR/debug/lib/libssl."* "$TARGET_DIR/bin/$OS/debug/"
+    cp -r "$BUILD_DIR/release/lib/libcrypto."* "$TARGET_DIR/bin/$OS/release/"
+    cp -r "$BUILD_DIR/release/lib/libssl."* "$TARGET_DIR/bin/$OS/release/"
     cp -r "$BUILD_DIR/release/include"/* "$TARGET_DIR/include"
 }
 
@@ -110,7 +166,7 @@ echo "Unzip"
 doUnzip | indent
 
 echo "Build"
-doBuild 2>&1 | indent
+"doBuild${OS^}" 2>&1 | indent
 
 echo "Copy"
 doCopy | indent
