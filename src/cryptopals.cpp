91ce8cbc68a862f70af4c5f05199271d2abac754
Samuel Albrecht <elsamuko@gmail.com>
2019-06-18 22:23:49 +0200

    chg: run all or single challenges

--- src/cryptopals.cpp
+++ src/cryptopals.cpp
@@ -1,22 +1,48 @@
 #include "set1.hpp"
 #include "set2.hpp"
 
-int main() {
+#include <map>
+#include <functional>
 
-    // https://cryptopals.com/sets/1
-    challenge1_1();
-    challenge1_2();
-    challenge1_3();
-    challenge1_4();
-    challenge1_5();
-    challenge1_6();
-    challenge1_7();
-    challenge1_8();
+#include "log.hpp"
 
-    // https://cryptopals.com/sets/2
-    challenge2_9();
-    challenge2_10();
-    challenge2_11();
+int main( int argc, char* argv[] ) {
+
+    const std::map<std::string, std::function<void()>> challenges = {
+
+        // https://cryptopals.com/sets/1
+        { "1.1", challenge1_1 },
+        { "1.2", challenge1_2 },
+        { "1.3", challenge1_3 },
+        { "1.4", challenge1_4 },
+        { "1.5", challenge1_5 },
+        { "1.6", challenge1_6 },
+        { "1.7", challenge1_7 },
+        { "1.8", challenge1_8 },
+
+        // https://cryptopals.com/sets/2
+        { "2.9", challenge2_9 },
+        { "2.10", challenge2_10 },
+        { "2.11", challenge2_11 },
+
+    };
+
+    // run one challenge
+    if( argc == 2 ) {
+        auto it = challenges.find( argv[1] );
+
+        if( it != challenges.cend() ) {
+            it->second();
+        } else {
+            LOG( argv[1] << " not found" );
+        }
+    }
+    // run all challenges
+    else {
+        for( auto&& it : challenges ) {
+            it.second();
+        }
+    }
 
     return 0;
 }
