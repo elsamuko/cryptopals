59972193c64e946c5d3d33e995b89c2826b777cc
Samuel Albrecht <elsamuko@gmail.com>
2019-12-04 23:09:07 +0100

    add: str(), bytes() to convert Bytes <> std::string

--- src/types.hpp
+++ src/types.hpp
@@ -5,3 +5,11 @@
 
 using Bytes = std::vector<uint8_t>;
 using Byte = uint8_t;
+
+inline std::string str( const Bytes& bytes ) {
+    return std::string( bytes.cbegin(), bytes.cend() );
+}
+
+inline Bytes bytes( const std::string& string ) {
+    return Bytes( string.cbegin(), string.cend() );
+}
