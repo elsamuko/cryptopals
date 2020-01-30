c3db79b8c795472d3bd549180331c7525d0fac85
Samuel Albrecht <elsamuko@gmail.com>
2020-01-30 17:50:48 +0100

    chg: upgrade test.sh

--- utils/web/test.sh
+++ utils/web/test.sh
@@ -3,16 +3,23 @@
 BASE="http://localhost:9000"
 URL_BAD="$BASE/test?file=foo&signature=46b4ec586117154dacd49d664e5d63fdc88efb51"
 URL_GOOD="$BASE/test?file=foo&signature=fa9908c7e2e1dfe6917b19ccfc04998ead09aef9"
+URL_SHORT="$BASE/short?file=foo&signature=fa9908c7e2e1dfe6917b19ccfc04998ead09aef9"
 
-function status {
-    grep "< HTTP/1.1" | grep -Po '\d{3}'
+function http_GET {
+    curl -vs4 "$1" 2>&1 >/dev/null | grep "< HTTP/1.1" | grep -Po '\d{3}'
 }
 
-echo -n "ok        : "
-curl -vs4 "$BASE/ok"     2>&1 >/dev/null | status
-echo -n "error     : "
-curl -vs4 "$BASE/error"  2>&1 >/dev/null | status
-echo -n "test bad  : "
-curl -vs4 "$URL_BAD"     2>&1 >/dev/null | status
-echo -n "test good : "
-curl -vs4 "$URL_GOOD"    2>&1 >/dev/null | status
+echo -n "ok         : "
+http_GET "$BASE/ok"
+
+echo -n "error      : "
+http_GET "$BASE/error"
+
+echo -n "test bad   : "
+http_GET "$URL_BAD"
+
+echo -n "test good  : "
+http_GET "$URL_GOOD"
+
+echo -n "test short : "
+http_GET "$URL_SHORT"
